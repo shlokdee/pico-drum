f5f6330c3e5688f98b1a9de776337b73b0b8206f
shlokdee <f20260342@goa.bits-pilani.ac.in>
2026-09-19 12:37:33 +0530

    Integrate Adafruit MIDI and update GPIO setup
    Refactor firmware to use Adafruit MIDI library for note handling and update pin configurations.

--- firmware.py
+++ firmware.py
@@ -1,14 +1,35 @@
 import time
-from machine import Pin, ADC
+import board
+import digitalio
+import analogio
+import usb_midi
+import adafruit_midi
 
-s0=Pin(16, Pin.OUT)
-s1=Pin(17, Pin.OUT)
-s2=Pin(18, Pin.OUT)
+from adafruit_midi.note_on import NoteOn
+from adafruit_midi.note_off import NoteOff
+from adafruit_midi.control_change import ControlChange
 
-adc=ADC(Pin(26))
+midi = adafruit_midi.MIDI(midi_out=usb_midi.ports[1], out_channel=0)
+
+pad_notes=[36,38,42,46,49,52,45,48]
+
+pad_hit=[False]*8
+
+threshhold=5000
+
+s0 = DigitalInOut(board.GP16)
+s0.direction = Direction.OUTPUT
+
+s1 = DigitalInOut(board.GP17)
+s1.direction = Direction.OUTPUT
+
+s2 = DigitalInOut(board.GP18)
+s2.direction = Direction.OUTPUT
+
+adc = analogio.AnalogIn(board.GP26)
 
 def map_values(x):
-  return int((x*127)/65535)
+  return min(127, max(1, x // 512))
 
 
 def selection(x):
@@ -18,11 +39,17 @@ def selection(x):
 
 
 while True:
-  lis=[]
   for i in range(8):
     selection(i)
     time.sleep_us(100)
-    lis.append(map_values(adc.read_u16()))
-  print(lis)
+    x=adc.value()
+    if x>threshhold and not pad_hit[i]:
+        vel=map_values(x)
+        midi.send(NoteOn(pad_notes[i], vel))
+        pad_hit=True
+    elif x<threshhold and pad_hit[i]
+        midi.send(NoteOff(pad_notes[i], 0))
+        pad_hit=False
   time.sleep(1)
+  time.sleep_us(10)
 
