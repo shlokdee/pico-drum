3e29ba43ad9ebdb439ea9cb6526f237bd82d1148
shlokdee <f20260342@goa.bits-pilani.ac.in>
2026-09-21 17:44:28 +0530

    removed time.sleep that was remaining from previous test version
    also added comments for better readibility

--- firmware.py
+++ firmware.py
@@ -11,30 +11,30 @@ from adafruit_midi.control_change import ControlChange
 
 midi = adafruit_midi.MIDI(midi_out=usb_midi.ports[1], out_channel=0)
 
-pad_notes=[36,38,42,46,49,52,45,48]
+pad_notes=[36,38,42,46,49,52,45,48]  #generic drum midi notes, edit this to put your own, or map in the player software...
 
-pad_hit=[False]*8
+pad_hit=[False]*8  #checks whether the pad has already been hit, stops from sending multiple midi commands for the same hit
 
 threshhold=5000
 
+#declaring outputs
 s0 = DigitalInOut(board.GP16)
 s0.direction = Direction.OUTPUT
-
 s1 = DigitalInOut(board.GP17)
 s1.direction = Direction.OUTPUT
-
 s2 = DigitalInOut(board.GP18)
 s2.direction = Direction.OUTPUT
-
 adc = analogio.AnalogIn(board.GP26)
 
 
 while True:
   for i in range(8):
+    #selects the correct piezo by setting the proper values on the mux
     s0.value(i&1)
     s1.value((i>>1)&1)
     s2.value((i>>2)&1)
-    time.sleep_us(100)
+
+    time.sleep_us(100) #delay for mux to not glitch out
     x=adc.value()
     if x>threshhold and not pad_hit[i]:
         vel=min(127, max(1, x // 512))
@@ -43,6 +43,5 @@ while True:
     elif x<threshhold and pad_hit[i]
         midi.send(NoteOff(pad_notes[i], 0))
         pad_hit=False
-  time.sleep(1)
   time.sleep_us(10)
 
